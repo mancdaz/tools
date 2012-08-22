d9b3f4e225867cab23cacf65f365b68d5ac69e35
galstrom21 <jshepher@rackspace.com>
2012-08-21 20:14:35 -0500

    Adding build_swift_dev.sh script
    * creates 4 nodes on legacy rackspace cloud servers (using knife-rackspace)
      * 1st node is swift management server
      * 2nd, 3rd and 4th nodes are all swift storage nodes
    * executes multiple passes of chef-client runs to orchestrate cluster setup
    * storage nodes have 3 x 1GB loopback devices for swift disks

---
+++ build_swift_dev.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+# set -x
+set -u
+
+STORAGE_IMAGE=${STORAGE_IMAGE:-25784378}
+MGMT_IMAGE=${MGMT_IMAGE:-25762231}
+FLAVOR_1G_ID=${FLAVOR_1G_ID:-3}
+FLAVOR_2G_ID=${FLAVOR_2G_ID:-4}
+CHEF_ENV=${CHEF_ENV:-cs-swift}
+
+# Lets spin up the required nodes
+MGMT_NODE_NAME=${MGMT_NODE_NAME:-shep-swift-mgmt.rcbops.me}
+PROXY_NODE_NAME=${PROXY_NODE_NAME:-shep-swift-proxy.rcbops.me}
+STORAGE_NODE1_NAME=${STORAGE_NODE1_NAME:-shep-swift-storage1.rcbops.me}
+STORAGE_NODE2_NAME=${STORAGE_NODE2_NAME:-shep-swift-storage2.rcbops.me}
+STORAGE_NODE3_NAME=${STORAGE_NODE3_NAME:-shep-swift-storage3.rcbops.me}
+
+
+host_list=( $MGMT_NODE_NAME $STORAGE_NODE1_NAME $STORAGE_NODE2_NAME $STORAGE_NODE3_NAME )
+for host in ${host_list[@]}; do
+    knife node delete -y ${host} || true;
+    knife client delete -y ${host} || true;
+done
+
+
+echo "Booting MGMT Node"
+knife rackspace server create -N $MGMT_NODE_NAME -S $MGMT_NODE_NAME -f ${FLAVOR_2G_ID} -I $MGMT_IMAGE -E $CHEF_ENV -r role[mysql-master],role[keystone],role[swift-management-server],role[swift-proxy-server],recipe[exerstack],recipe[kong]
+
+echo "Booting Storate Node1"
+knife rackspace server create -N $STORAGE_NODE1_NAME -S $STORAGE_NODE1_NAME -f ${FLAVOR_1G_ID} -I $STORAGE_IMAGE -E $CHEF_ENV -r role[swift-object-server],role[swift-container-server],role[swift-account-server]
+
+echo "Setting swift zone on Storage Node1"
+knife exec -E "nodes.find(:name => '$STORAGE_NODE1_NAME') {|n| n.set['swift']['zone'] = '1'; n.save }"
+
+echo "Booting Storate Node2"
+knife rackspace server create -N $STORAGE_NODE2_NAME -S $STORAGE_NODE2_NAME -f ${FLAVOR_1G_ID} -I $STORAGE_IMAGE -E $CHEF_ENV -r role[swift-object-server],role[swift-container-server],role[swift-account-server]
+
+echo "Setting swift zone on Storage Node2"
+knife exec -E "nodes.find(:name => '$STORAGE_NODE2_NAME') {|n| n.set['swift']['zone'] = '2'; n.save }"
+
+echo "Booting Storate Node3"
+knife rackspace server create -N $STORAGE_NODE3_NAME -S $STORAGE_NODE3_NAME -f ${FLAVOR_1G_ID} -I $STORAGE_IMAGE -E $CHEF_ENV -r role[swift-object-server],role[swift-container-server],role[swift-account-server]
+
+echo "Setting swift zone on Storage Node3"
+knife exec -E "nodes.find(:name => '$STORAGE_NODE3_NAME') {|n| n.set['swift']['zone'] = '3'; n.save }"
+
+for i in 1 2 3; do
+    for host in ${host_list[@]}; do
+        echo "Running second passes on ${host}";
+        ssh root@$(knife node show ${host} | grep IP | awk '{print $2}') chef-client;
+    done
+done
